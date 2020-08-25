b6a29d9e1761c3062dac39864fb75f93371d448a
lighteagle <sofyan.agus18@gmail.com>
2020-08-25 22:37:56 +0700

    e-comerce landing page

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => nav.classList.toggle("show"));
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  // Active Link
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  // Remove menu mobile
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
